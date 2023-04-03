576649bdc6394470eb06bae47f6fb3fa58e7f291
Yasuki Kanamori <yasuki.kanamori.19901207@gmail.com>
2023-04-03 14:54:29 +0900

    create guest top

---
+++ resources/js/Pages/Guest/Top.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+  <Head title="TOP" />
+
+  <GuestLayout>
+    <div></div>
+  </GuestLayout>
+</template>
