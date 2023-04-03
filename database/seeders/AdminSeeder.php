84b21e89f5cda40a2ef4fd33f13a5e26dbeb0d2f
Yasuki Kanamori <yasuki.kanamori.19901207@gmail.com>
2023-04-03 13:43:32 +0900

    change from users to admins

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+            'name' => 'test',
+            'email' => 'test.taro@test.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
